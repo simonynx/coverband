f9db4986a09931b44449ba11c32e6b130acddc69
akeeton@andrew.cmu.edu <akeeton@andrew.cmu.edu@f16039bc-091a-11de-b55b-d397e7115e68>
2009-03-06 06:32:45 +0000

    Beginning work.

--- trunk/data_engine.py
+++ trunk/data_engine.py
@@ -3,6 +3,8 @@ from __future__ import division
 import pygame
 from OpenGL.GL import *
 
+from graphics import *
+
 class Repr:
 	"""
 	Supply a standard __repr__ string for all subclasses.
@@ -40,56 +42,6 @@ class GameEvent(Repr):
 		if tickcmp == 0:
 			return cmp(id(x), id(y))
 
-def QUAD_RECT_PRISM(x, y, z, xlen, ylen, zlen):
-	"""
-	A function that draws a rectangular prism with center at the point (x, y, z)
-	using quad surfaces.
-	"""
-	glBegin(GL_QUADS)
-
-	glPushMatrix()
-	glTranslate(x / 2.0, y / 2.0, z / 2.0)
-
-	# Front face
-	glVertex(0.0, 0.0, 0.0)		# Bottom left
-	glVertex(xlen, 0.0, 0.0)	# Bottom right
-	glVertex(xlen, ylen, 0.0)	# Top right
-	glVertex(0.0, ylen, 0.0)	# Top left
-
-	# Top face
-	glVertex(0.0, ylen, 0.0)	# Bottom left
-	glVertex(xlen, ylen, 0.0)	# Bottom right
-	glVertex(xlen, ylen, zlen)	# Top right
-	glVertex(0.0, ylen, zlen)	# Top left
-
-	# Back face
-	glVertex(xlen, 0.0, zlen)	# Bottom left
-	glVertex(0.0, 0.0, zlen)	# Bottom right
-	glVertex(0.0, ylen, zlen)	# Top right
-	glVertex(xlen, ylen, zlen)	# Top left
-
-	# Bottom face
-	glVertex(0.0, 0.0, zlen)	# Bottom left
-	glVertex(xlen, 0.0, zlen)	# Bottom right
-	glVertex(xlen, 0.0, 0.0)	# Top right
-	glVertex(0.0, 0.0, 0.0)		# Top left
-
-	# Right face
-	glVertex(xlen, 0.0, 0.0)	# Bottom left
-	glVertex(xlen, 0.0, zlen)	# Bottom right
-	glVertex(xlen, ylen, zlen)	# Top right
-	glVertex(xlen, ylen, 0.0)	# Top left
-
-	# Left face
-	glVertex(0.0, 0.0, zlen)	# Bottom left
-	glVertex(0.0, 0.0, 0.0)		# Bottom right
-	glVertex(0.0, ylen, 0.0)	# Top right
-	glVertex(0.0, ylen, zlen)	# Top left
-
-	glPopMatrix()
-
-	glEnd()
-
 class GLObject(Repr):
 	# The point (x, y, z) defines the center of the object.
 	(x, y, z) = (0.0, 0.0, 0.0)
@@ -103,6 +55,9 @@ class GLObject(Repr):
 		@type glCreationFunc: A function that draws OpenGL objects
 		@param *funcArgs: The arguments to glCreationFunc
 		"""
+		if pygame.display.get_init() == False:
+			raise pygame.error("Display must be initialized before using Opengl")
+
 		self.x = x
 		self.y = y
 		self.z = z
@@ -117,11 +72,11 @@ class GLObject(Repr):
 		self.dlNum = glGenLists(1)
 
 		glNewList(self.dlNum, GL_COMPILE)
-		glCreationFunc(*funcArgs)
+		glCreationFunc(self.x, self.y, self.z, *funcArgs)
 		glEndList
 	
 	def draw(self):
-		glCallList(displayListNum)
+		glCallList(self.dlNum)
 
 class Note(Repr, GameEvent):
 	(xlen, ylen, zlen) = (0.0, 0.0, 0.0)
@@ -129,13 +84,14 @@ class Note(Repr, GameEvent):
 
 	def __init__(self, tick = 0, x = 0.0, y = 0.0, z = 0.0,
 			xlen = 0.0, ylen = 0.0, zlen = 0.0):
-		GameEvent.__init__(tick)
+		GameEvent.__init__(self, tick)
 
 		self.xlen = xlen
 		self.ylen = ylen
 		self.zlen = zlen
 
 		self.glNote = GLObject(x, y, z, QUAD_RECT_PRISM, xlen, ylen, zlen)
+		print(self)
 
 	def draw(self):
 		self.glNote.draw()
