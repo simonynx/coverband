ab79c8e26c699169affeee048be16b9eb69370a6
akeeton@andrew.cmu.edu <akeeton@andrew.cmu.edu@f16039bc-091a-11de-b55b-d397e7115e68>
2009-03-12 05:26:55 +0000

    First attempt at Beat class

--- trunk/data_engine.py
+++ trunk/data_engine.py
@@ -1,6 +1,5 @@
 from __future__ import division
 
-
 import pygame
 from OpenGL.GL import *
 
@@ -43,6 +42,9 @@ class SortedList(Repr):
 		return self.items.__iter__()
 
 	def add(self, item):
+		"""
+		Add an item to the sorted list using binary search.
+		"""
 		if len(self) == 0:
 			self.items.append(item)
 			return
@@ -78,6 +80,11 @@ class SortedList(Repr):
 	def remove(self, item):
 		self.items.remove(item)
 
+class Color:
+	red = (1.0, 0.0, 0.0)
+	white = (1.0, 1.0, 1.0)
+	gray = (0.5, 0.5, 0.5)
+
 class GameEvent(Repr):
 	"""
 	The main base class for all game events.
@@ -96,12 +103,12 @@ class GameEvent(Repr):
 			return cmp(id(self), id(other))
 
 class GLObject(Repr):
-	# The point (x, y, z) defines the center of the object.
-	(x, y, z) = (0.0, 0.0, 0.0)
 	numDLists = 0
 	displayList = 0
+	glCreationFunc = None
+	created = False
 
-	def __init__(self, x = 0.0, y = 0.0, z = 0.0, numDLists = 0):
+	def __init__(self, glCreationFunc, numDLists):
 		"""
 		@param glCreationFunc: This function will be used to "draw" something
 			to an OpenGL display list.
@@ -109,77 +116,141 @@ class GLObject(Repr):
 		@param *funcArgs: The arguments to glCreationFunc
 		"""
 		if not pygame.display.get_init():
-			raise pygame.error("Display must be initialized before using Opengl")
+			raise pygame.error("Display must be initialized before using OpenGL")
 
-		self.x = x
-		self.y = y
-		self.z = z
 		self.numDLists = numDLists
 		self.displayList = glGenLists(self.numDLists)
+		self.glCreationFunc = glCreationFunc
+
+		# createGLDisplayList must be called before draw.
+		self.created = False
 
 	def __del__(self):
 		if self.displayList != 0:
 			glDeleteLists(self.displayList, self.numDLists)
 	
-	def createGLDisplayList(self, dListNum, glCreationFunc, *funcArgs):
+	def createGLDisplayList(self, dListNum, *funcArgs):
 		glNewList(self.displayList + dListNum, GL_COMPILE)
-		glCreationFunc(self.x, self.y, self.z, *funcArgs)
+		self.glCreationFunc(*funcArgs)
 		glEndList()
+
+		self.created = True
 	
-	def draw(self, dListNum):
-		glCallList(self.displayList + dListNum)
+	def draw(self):
+		if not self.created:
+			raise Exception("Must call createGLDisplayList before calling draw")
+
+		for i in range(self.numDLists):
+			glCallList(self.displayList + i)
 
 class Note(Repr, GameEvent, GLObject):
-	(xlen, ylen, zlen) = (0.0, 0.0, 0.0)
+	"""
+	@param position: A fraction representing where on the chart to place the note.
+		For example, a note with position 3/8 would be the third eight-note.
+	"""
+	color = "notacolor"
+	position = 0.0
 
-	def __init__(self, tick, x, y, z, xlen = 1.0, ylen = 1.0, zlen = 1.0):
+	def __init__(self, tick, color, position):
 		GameEvent.__init__(self, tick)
-		GLObject.__init__(self, x, y, z, numDLists = 1)
+		GLObject.__init__(self, GL_QUAD_RECT_PRISM, numDLists = 1)
 
-		self.xlen = xlen
-		self.ylen = ylen
-		self.zlen = zlen
-
-		self.createGLDisplayList(0, QUAD_RECT_PRISM, xlen, ylen, zlen)
-
-		print(self)
-
-	def draw(self):
-		GLObject.draw(self, 0)
+		self.color = color
+		self.position = position
+		# The enclosing Beat object needs to call createGLDisplayList.
 
 def BEATS_PER_SECOND(bpm):
 	return bpm * 60.0
 
 class Beat(Repr, GLObject):
 	"""
-	The Beat class is where most of the events (Notes) will go.
+	The Beat class is all of the notes will go.
 	@param bpm: BPM for this beat.
 	@type bpm: positive integer
-	@param eventsList: Sorted list of events local to this beat.
-	@param (x, y, z): Center of the base of the beat.
+	@param notesList: Sorted list of notes local to this beat.
 	"""
-	bpm = 120
-	eventsList = None
+	bpm = 0
+	notesList = None
+	width = 0.0
+	height = 0.0
+	wLane = 0.0
 
-	(x, y, z) = (1.0, 1.0, 0.0)
-	height = 1.0
-	width = W_CHART
-
-	def __init__(self, bpm, x, y, *events):
-		GLObject.__init__(self, x, y, 0.0)
+	def __init__(self, bpm, *notes):
+		GLObject.__init__(self, self.GL_BEAT, numDLists = 1)
 
 		self.bpm = bpm
-		self.eventsList = SortedList(events)
+		self.notesList = SortedList(notes)
 		self.width = W_CHART
 		self.height = SPD_CHART / BEATS_PER_SECOND(bpm)
+		numLanes = self.numLanes()
+		self.wLane = (W_CHART - (numLanes + 1) * W_LINE) / numLanes
 
+		self.createGLDisplayList(1)
 
+		for note in notes:	
+			wLane = self.wLane
+			# Multiply by 4 since each beat is the beginning of a quarter note.
+			yNote = 4.0 * note.position * self.height
+			noteLane = self.noteLane(note.color)
+
+			if noteLane() > 0:
+				xNote = (noteLane - 1) * wLane + (noteLane * W_LINE)
+				wNote = wLane
+				hNote = H_FAT_NOTE
+			else:
+				xNote = 0.0
+				wNote = W_CHART
+				hNote = H_SKINNY_NOTE
+
+			note.createGLDisplayList(1, xNote, yNote, 0.0, wNote, hNote, hNote)
+
+	def numLanes(self):
+		"""
+		Abstract class.
+		"""
+		pass
+	def noteLane(self, color):
+		"""
+		Abstract class.
+		"""
+		pass
+
+	def GL_BEAT(self):
+		numLanes = self.numLanes()
+
+		# Draw the vertical lines that define the lanes.
+		glColor(Color.white)
+		for i in range(numLanes + 1):
+			GL_QUAD_RECT_PRISM(i * self.wLane, 0.0, 0.0,
+				W_LINE, self.height, W_LINE)
+
+		# Draw the full-beat horizontal line.
+		GL_QUAD_RECT_PRISM(0.0, 0.0, 0.0, self.width, W_LINE, W_LINE)
+
+		# Draw the half-beat horizontal line.
+		glColor(Color.gray)
+		GL_QUAD_RECT_PRISM(0.0, self.height / 2.0, 0.0,
+			self.width, W_LINE, W_LINE)
+
+class DrumsBeat(Beat):
+	def __init__(self, bpm, *notes):
+		Beat.__init__(self, bpm, *notes)
+
+	def numLanes(self):
+		return 4
+
+	def noteLane(self, color):
+		"""
+		Return the lane that the note goes in.  Lane 0 is the whole chart.
+		"""
+
+		return { "red": 1, "yellow": 2, "blue": 3, "green": 4, "orange": 0 }[color]
 if __name__ == "__main__":
 	import random
-	sorted = SortedList()
+	sortedList = SortedList()
 
-	unsorted = [random.randint(0, 10) for x in range(20)]
+	unsortedList = [random.randint(0, 10) for x in range(20)]
 
-	map(sorted.add, unsorted)
+	map(sortedList.add, unsortedList)
 
-	print("unsorted: %s\nsorted: %s" % (unsorted, sorted.items))
+	print("unsorted: %s\nsorted: %s" % (unsortedList, sortedList.items))
